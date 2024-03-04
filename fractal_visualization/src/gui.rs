dec807e39c3037152ab7536590d7e9f083d3a4d1
David Riva <davidjriva@gmail.com>
2024-03-03 22:30:59 -0700

    abstract the GUI

---
+++ fractal_visualization/src/gui.rs
@@ -0,0 +1,40 @@
+use minifb::{Key, Window, WindowOptions};
+
+pub struct Gui {
+    window: Window,
+    width: usize,
+    height: usize,
+    buffer: Vec<u32>
+}
+
+impl Gui {
+    pub fn new (width: usize, height: usize) -> Gui {
+        Gui {
+            width,
+            height,
+            window: Window::new(
+            "Mandelbrot Visualization",
+            width,
+            height,
+            WindowOptions::default()
+            ).unwrap_or_else(|e| panic!("{}", e)),
+            buffer: Vec::new()
+        }
+    }
+
+    pub(crate) fn set_buffer(&mut self, buffer: Vec<u32>) {
+        self.buffer = buffer;
+    }
+
+    pub fn update_window(&mut self) {
+        self.window.update_with_buffer(&self.buffer, self.width, self.height).unwrap()
+    }
+
+    pub fn window_is_open(&mut self) -> bool {
+        return self.window.is_open();
+    }
+
+    pub fn escape_key_pressed(&mut self) -> bool {
+        return self.window.is_key_down(Key::Escape);
+    }
+}
--- fractal_visualization/src/main.rs
+++ fractal_visualization/src/main.rs
@@ -1,7 +1,7 @@
 mod palettes;
+mod gui;
 
 use image::{Rgba, RgbaImage};
-use minifb::{Key, Window, WindowOptions};
 use std::time::{Instant};
 
 // STATIC VARIABLES
@@ -148,19 +148,15 @@ fn main() {
     let elapsed_ms = elapsed_time.as_secs() * 1000 + u64::from(elapsed_time.subsec_millis());
     println!("Elapsed time in milliseconds: {} ms", elapsed_ms);
 
-    let mut window = Window::new(
-        "Mandelbrot Visualization",
-        x_pixel_upper_bound,
-        y_pixel_upper_bound,
-        WindowOptions::default()
-    ).unwrap_or_else(|e| panic!("{}", e));
+    let mut gui = gui::Gui::new(x_pixel_upper_bound, y_pixel_upper_bound);
 
-    let buffer: Vec<u32> = mandelbrot_image
+    gui.set_buffer(mandelbrot_image
         .pixels()
         .map(|pixel| pixel[0] as u32 | (pixel[1] as u32) << 8 | (pixel[2] as u32) << 16 | (pixel[3] as u32) << 24)
-        .collect();
+        .collect()
+    );
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        window.update_with_buffer(&buffer, x_pixel_upper_bound, y_pixel_upper_bound).unwrap();
+    while gui.window_is_open() && !gui.escape_key_pressed() {
+        gui.update_window();
     }
 }
